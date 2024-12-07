8ff85d272213a33f24ae77254e385e1e0cc0eaf4
agenthah <ashmithandoo@outlook.com>
2024-12-07 20:33:05 +0000

    commit

--- Projects/Python_Platformer/main.py
+++ Projects/Python_Platformer/main.py
@@ -14,6 +14,40 @@ PLAYER_VEL = 5
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
+
+class Player(pygame.sprite.sprite):
+    COLOR = (255, 0, 0)
+
+    def __init__(self, x, y, width, height):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.x_vel = 0
+        self.y_vel = 0
+        self.mask = None
+        self.direction = "left"
+        self.animation_count = 0
+
+    def move(self, dx, dy):
+        self.rect.x += dx
+        self.rect.y += dy
+    
+    def move_left(self, vel):
+        self.x_vel = -vel
+        if self.direction != "left":
+            self.direction = "left"
+            self.animation_count - 0
+
+    def move_right(self, vel):
+        self.x_vel = vel
+        if self.direction != "right":
+            self.direction = "right"
+            self.animation_count - 0
+
+    def loop(self, fps):
+        self.move(self.x_vel, self.y_vel)
+    
+    def draw(self, win):
+        pygame.draw.rect(win, self.COLOR, self.rect)
+
 def get_background(name):
     image = pygame.image.load(join('Projects','Python_Platformer','assets', 'Background', name))
     __, __, width, height = image.get_rect()
